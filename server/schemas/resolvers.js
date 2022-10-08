d1a32dffb7198532b8e8c32510d549ff12edcbcd
Marcela Mejia <mejiao24@hotmail.com>
2022-10-08 17:13:56 +1100

    Implement addUser resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,22 +10,31 @@ const resolvers = {
   },
   Mutation: {
     login: async (parent, { email, password }) => {
-        const user = await User.findOne( { email });
-  
-        if (!user) {
-          throw new AuthenticationError("Can't find this user");
-        }
-  
-        const correctPw = await user.isCorrectPassword(password);
-  
-        if (!correctPw) {
-          throw new AuthenticationError('Wrong password!');
-        }
-  
-        const token = signToken(user);
-  
-        return { token, user };
+      const user = await User.findOne( { email });
+
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Wrong password!');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
     },
+    addUser: async ( parent, { username, email, password } ) => { 
+      const user = await User.create({ username, email, password });
+  
+      if (!user) {
+        throw new AuthenticationError('Something is wrong');
+      }
+      const token = signToken(user);
+      return { token, user };
+    }
   },
 };
 
