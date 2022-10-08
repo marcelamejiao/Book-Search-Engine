0228945d061bcde7eb0f1376663d8fad3c6af5df
Marcela Mejia <mejiao24@hotmail.com>
2022-10-08 16:24:02 +1100

    Implement typeDefs and login resolver

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,32 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async () => {
+      return User.find({});
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne( { email });
+  
+        if (!user) {
+          throw new AuthenticationError("Can't find this user");
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Wrong password!');
+        }
+  
+        const token = signToken(user);
+  
+        return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
