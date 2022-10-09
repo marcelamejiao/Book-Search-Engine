d60858177a7b731297b4db6570ce31d74bb20177
Marcela Mejia <mejiao24@hotmail.com>
2022-10-09 18:44:54 +1100

    Add mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,20 +4,51 @@ export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
             token
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation AddUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
             user {
-              _id
-              username
-              email
-              bookCount
-              savedBooks {
+                username
+                _id
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation Mutation($input: saveBookInput) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
                 bookId
                 authors
                 description
                 title
                 image
                 link
-              } 
             }
         }
     }
 `;
+
+export const REMOVE_BOOK = gql`
+    mutation Mutation($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+            }
+        }
+    }
+`;
+
